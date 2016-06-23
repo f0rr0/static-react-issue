494844e418f7fefa60e29be2d1857dc33354bc82
Sid <sid26@ucla.edu>
2016-06-22 20:05:06 -0700

    initial

---
+++ app/index.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { renderToString } from 'react-dom/server';
+import { Router, Route, browserHistory,
+  IndexRoute, Link, createMemoryHistory, match, RoutingContext } from 'react-router';
+
+const createElement = (Component, props) => <Component key={new Date().getTime()} {...props} />;
+
+const App = ({ children }) => <div className="app">{children}</div>;
+const About = () => <div>About</div>;
+const Home = () => {
+  return (
+    <main>
+      <div>
+        <p>Stuff</p>
+        <Link to="/about">Click</Link>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+      </div>
+    </main>
+  );
+};
+
+const routes = (
+  <Route path="/" component={App}>
+    <IndexRoute component={Home} />
+    <Route path="/about" component={About} />
+  </Route>
+);
+
+const run = () => {
+  ReactDOM.render(
+    <Router history={browserHistory} routes={routes} createElement={createElement} />,
+    document.getElementById('root')
+  );
+};
+
+
+const loadedStates = ['complete', 'loaded', 'interactive'];
+
+if (typeof document !== 'undefined') {
+  if (loadedStates.includes(document.readyState) && document.body) {
+    run();
+  } else {
+    window.addEventListener('DOMContentLoaded', run, false);
+  }
+}
+
+export default (locals, callback) => {
+  const history = createMemoryHistory();
+  const location = history.createLocation(locals.path);
+  match({ routes, location }, (error, redirectLocation, renderProps) => {
+      callback(null, renderToString(<RoutingContext {...renderProps} />)});
+    }
+  );
+};
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const { resolve } = require('path');
+const StaticWebpackPlugin = require('static-site-generator-webpack-plugin');
+
+const data = {
+  routes: [
+    '/',
+    '/about'
+  ]
+};
+
+module.exports = env => {
+  return {
+    context: resolve(__dirname, './app'),
+    entry: {
+      app: './index.jsx'
+    },
+    output: {
+      filename: '[name].[chunkhash].bundle.js',
+      path: resolve(__dirname, './dist'),
+      pathInfo: !env.prod,
+      chunkFilename: '[chunkhash].js',
+      libraryTarget: 'umd'
+    },
+    devtool: env.prod ? 'source-map' : 'eval',
+    module: {
+      loaders: [
+        {
+          test: /\.css$/,
+          loaders: [
+            'style',
+            'css'
+          ]
+        },
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          loaders: [
+            'babel-loader'
+          ]
+        }
+      ],
+    },
+    plugins: [
+      new StaticWebpackPlugin('app', data.routes)
+    ],
+    resolve: {
+      extensions: ['', '.js', '.jsx'],
+      modules: [
+        resolve('./app'),
+        'node_modules'
+      ]
+    },
+    devServer: {
+      contentBase: './app'
+    }
+  };
+};
